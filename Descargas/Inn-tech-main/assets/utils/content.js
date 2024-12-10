d608c61034ff65397c2a6f6b9c20474e69a7e18a
jivagrisma <jivagrisma@gmail.com>
2024-12-09 21:17:35 -0500

    se guardan cambios realizados

---
+++ Descargas/Inn-tech-main/assets/utils/content.js
@@ -0,0 +1,110 @@
+const clients = [
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/facebook.31d5f92.png",
+    alt: "Facebook"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/google.517da09.png",
+    alt: "Google"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/netflix.e3ad953.png",
+    alt: "Netflix"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/paypal.ec56157.png",
+    alt: "Paypal"
+  },
+];
+
+const explore = [
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/graphics-design.d32a2f8.svg",
+    alt: "Diseño Gráfico"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/online-marketing.74e221b.svg",
+    alt: "Marketing Digital"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/writing-translation.32ebe2e.svg",
+    alt: "Redacción y traducción"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/video-animation.f0d9d71.svg",
+    alt: "Video & Animación"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/music-audio.320af20.svg",
+    alt: "Música & Audio"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/programming.9362366.svg",
+    alt: "Programación y Tecnología"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/business.bbdf319.svg",
+    alt: "Negocios"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/lifestyle.745b575.svg",
+    alt: "Estilo de vida"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/data.718910f.svg",
+    alt: "Datos"
+  },
+];
+
+const services = [
+  {
+    url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741668/seo-2x.png",
+    moto: "Desbloquee el crecimiento en línea",
+    head: "SEO"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741664/illustration-2x.png",
+    moto: "Colorea tus sueños",
+    head: "Ilustración"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741674/translation-2x.png",
+    moto: "Global",
+    head: "Traducción"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741664/data-entry-2x.png",
+    moto: "Conozca su negocio",
+    head: "Análisis de datos"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/book-covers-2x.png",
+    moto: "Muestra tu historia",
+    head: "Portadas de libros"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/logo-design-2x.png",
+    moto: "Construye tu marca",
+    head: "Diseño de logotipo"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png",
+    moto: "Personaliza tu sitio",
+    head: "Wordpress"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741669/voiceover-2x.png",
+    moto: "Comparte tu mensaje",
+    head: "Voz en off"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png",
+    moto: "Involucre a su audiencia",
+    head: "Vídeo explicativo"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png",
+    moto: "Llegar a más clientes",
+    head: "Social Media"
+  },
+]
